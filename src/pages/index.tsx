2dbc547b5e7f64db84f5fe675dde72154af0b64a
Henrique <ishenriquealbert@gmail.com>
2021-11-21 20:57:50 -0300

    :sparkles: Add basic layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,13 @@
+import { Button, Flex, Heading } from '@chakra-ui/react'
 import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
   return (
-    <div>
-      <h1>Toud app</h1>
-    </div>
+    <Flex direction='column' h='100vh' align='center' justify='center'>
+      <Heading mb={10}>Toud App</Heading>
+      <Button w='400px' mb={4}>Login</Button>
+      <Button variant='outline' w='400px'>Sign in</Button>
+    </Flex>
   )
 }
 
