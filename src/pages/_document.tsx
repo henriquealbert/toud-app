8f22cfd4354954a593cb9ceccbfdb947e7a4ec23
hschmaiske <ishenriquealbert@gmail.com>
2022-05-15 18:10:15 -0300

    add favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,4 @@
-import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document'
+import Document, { DocumentContext, Head, Html, Main, NextScript } from 'next/document'
 
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
@@ -16,6 +16,9 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;700&display=swap"
             rel="stylesheet"
           />
+          <link rel="apple-touch-icon" sizes="152x152" href="/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         </Head>
         <body>
           <Main />
