ce4f1c8beb010f3968c261ea0533db6f043d54df
hschmaiske <ishenriquealbert@gmail.com>
2022-06-01 18:12:11 -0300

    update favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -16,9 +16,7 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;700&display=swap"
             rel="stylesheet"
           />
-          <link rel="apple-touch-icon" sizes="152x152" href="/apple-touch-icon.png" />
-          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="shortcut icon" href="favicon.png" type="image/x-icon" />
         </Head>
         <body>
           <Main />
