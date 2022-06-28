d8ab9e61740931926d0ffb863e732746fd305f37
hschmaiske <ishenriquealbert@gmail.com>
2022-06-28 15:14:18 -0300

    replace favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -18,7 +18,7 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;700&display=swap"
             rel="stylesheet"
           />
-          <link rel="shortcut icon" href="favicon.png" type="image/x-icon" />
+          <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
         </Head>
         <body>
           <GoogleTagManagerBody />
